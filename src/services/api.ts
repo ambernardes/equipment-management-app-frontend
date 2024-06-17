f8ebbb5c2657745db8c1bcd36621860fc6dd6e97
Antônio Marcos Machado Bernardes <a.machado.bernardes@accenture.com>
2024-06-17 17:49:34 -0300

    feat: change in base url

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 import type { AxiosInstance } from 'axios'
 
 const api: AxiosInstance = axios.create({
-  baseURL: 'https://localhost:44397'
+  baseURL: 'http://localhost:5062'
 })
 
 export default api
